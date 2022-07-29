e6e85b08db7dfd26ac89ddb49cb8b4bb80667c07
Taylor-Stacey <taylor88stacey@gmail.com>
2022-07-29 16:19:56 -0700

    WIP

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -3,15 +3,27 @@ const { JWT_SECRET } = require('../secrets');
 
 // AUTHENTICATION
 const restrict = (req, res, next) => {
-  jwt.verify(req.headers.authorization, JWT_SECRET, (err, decodedToken) => {
-    if(err) {
-      res.status(401).json({ message: 'user must be logged in' });
-      return;
-    }
+  // jwt.verify(req.headers.authorization, JWT_SECRET, (err, decodedToken) => {
+  //   if(err) {
+  //     res.status(401).json({ message: 'user must be logged in' });
+  //     return;
+  //   }
 
-    req.jwt = decodedToken;
-    next();
-  });
+  //   req.jwt = decodedToken;
+  //   next();
+  // });
+  const token = req.headers.authorization
+  if (!token) {
+    return next({ status: 401, message: 'Token required' })
+  }
+  jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+    if (err) {
+      next({ status: 401, message: 'Token invalid' })
+    } else {
+      req.decodedToken = decodedToken
+      next()
+    }
+  })
 }
 
 // AUTHORIZATION
